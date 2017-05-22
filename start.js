8f48af5d11f0e466fecb909b6d343c2abd9ee8c8
Axel <axelfuh@gmail.com>
2017-05-22 18:55:10 +0200

    .

--- start.js
+++ start.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-console */
 require('dotenv').config({ path: 'variables.env' });
 
-const chalk = require('chalk');
 const app = require('./app');
 
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
-  console.log(chalk.yellow.bold(`Express running → PORT ${server.address().port}`));
+  console.log('\x1b[33m%s\x1b[0m', `Express running → PORT ${server.address().port}`);
 });
